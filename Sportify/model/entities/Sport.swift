17c8ae37ae74068eea2a514f5fcc82bdd9221147
ahmed-sala <ahmeds66210@gmail.com>
2026-05-08 02:01:33 +0300

    feat: Add sports collection view to home screen

---
+++ Sportify/model/entities/Sport.swift
@@ -0,0 +1,13 @@
+//
+//  Sport.swift
+//  Sportify
+//
+//  Created by Ahmed Salah on 08/05/2026.
+//
+
+struct Sport {
+    let name: String
+    let image: String
+}
+
+
--- Sportify/modules/home/view/SportsViewController.swift
+++ Sportify/modules/home/view/SportsViewController.swift
@@ -16,75 +16,131 @@ class SportsViewController: UIViewController {
 
         setupCollectionView()
     }
-
+    let sportsData = [
+        Sport(name: "Football", image: "Football"),
+        Sport(name: "Tennis", image: "Tennis"),
+        Sport(name: "Basketball", image: "BascketBall"),
+        Sport(name: "Cricket", image: "Cracket")
+    ]
     private func setupCollectionView() {
 
-        // Cells
         homeCollectionView.register(
             UINib(nibName: "LiveResultCollectionViewCell", bundle: nil),
             forCellWithReuseIdentifier: "liveScore"
         )
 
         homeCollectionView.register(
-            UICollectionViewCell.self,
+            UINib(nibName: "SportsTypeCollectionViewCell", bundle: nil),
             forCellWithReuseIdentifier: "sports"
         )
 
         homeCollectionView.collectionViewLayout = createLayout()
         homeCollectionView.dataSource = self
+        homeCollectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
     }
     func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
 
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .estimated(250) // 🔥 dynamic height
-        )
+            if sectionIndex == 0 {
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .estimated(250)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.85),
+                    heightDimension: .estimated(250)
+                )
+                let group = NSCollectionLayoutGroup.vertical(
+                    layoutSize: groupSize,
+                    subitems: [item]
+                )
 
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0)
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.interGroupSpacing = 12
+                section.contentInsets = NSDirectionalEdgeInsets(top: 24, leading: 16, bottom: 24, trailing: 16)
 
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(0.85),
-            heightDimension: .estimated(250)
-        )
+                return section
+            }  else {
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
 
-        let group = NSCollectionLayoutGroup.vertical(
-            layoutSize: groupSize,
-            subitems: [item]
-        )
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(200))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
-        let section = NSCollectionLayoutSection(group: group)
+                let section = NSCollectionLayoutSection(group: group)
 
-        section.orthogonalScrollingBehavior = .groupPaging
-        section.interGroupSpacing = 8
-        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
 
-        return UICollectionViewCompositionalLayout(section: section)
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(30))
+                let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+
+                sectionHeader.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0)
+                section.boundarySupplementaryItems = [sectionHeader]
+
+                return section
+            }
+        }
     }
 }
 
-
 extension SportsViewController: UICollectionViewDataSource {
 
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return section == 0 ? 5 : sportsData.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "liveScore",
-            for: indexPath
-        ) as! LiveResultCollectionViewCell
 
-        let homeGoals = ["Salah 23'", "Trezeguet 55'"]
+        if indexPath.section == 0 {
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "liveScore",
+                for: indexPath
+            ) as! LiveResultCollectionViewCell
 
-        let awayGoals = indexPath.item == 0
+            let homeGoals = ["Salah 23'", "Trezeguet 55'"]
+
+            let awayGoals = indexPath.item == 0
             ? ["Messi 10'"]
             : ["Messi 10'", "Mbappe 30'", "Neymar 70'"]
 
-        cell.configure(homeGoals: homeGoals, awayGoals: awayGoals)
+            cell.configure(homeGoals: homeGoals, awayGoals: awayGoals)
+            return cell
+        }
+        else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sports", for: indexPath) as! SportsTypeCollectionViewCell
+            let sport = sportsData[indexPath.row]
 
-        return cell
+            cell.sportText.text = sport.name
+            cell.sportImage.image = UIImage(named: sport.image)
+
+            return cell
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+
+        if kind == UICollectionView.elementKindSectionHeader && indexPath.section == 1 {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+
+            header.subviews.forEach { $0.removeFromSuperview() }
+            let label = UILabel(frame: header.bounds)
+            label.text = "Sports"
+            label.font = .boldSystemFont(ofSize: 24)
+
+            header.addSubview(label)
+
+            return header
+        }
+        return UICollectionReusableView()
     }
 }
+
